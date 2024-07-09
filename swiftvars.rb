d28011355df27c15a49a827d02333b8068d11e0e
Marvin Lee Kobert <marvin.kobert@me.com>
2024-07-09 08:53:45 +0200

    fix: brew formula

--- swiftvars.rb
+++ swiftvars.rb
@@ -1,14 +1,15 @@
-class swiftvars < Formula
+class Swiftvars < Formula
   desc "Create your swift and kotlin UI theme templates from Figma JSON"
   homepage "https://github.com/treboc/homebrew-swiftvars"
-  url "https://github.com/treboc/homebrew-swiftvars/archive/v1.0.0.tar.gz"
+  url "https://github.com/treboc/homebrew-swiftvars/releases/download/1.0.0/v1.0.0.tar.gz"
   sha256 "41b5ec93841ef6c2556edb9c2e5697ba6c2db3ecc8df4204a63aca0e1022e234"
   version "1.0.0"
 
   depends_on :xcode => ["15.0", :build]
 
   def install
-    system "swift", "build", "-c", "release"
+    system "swift", "package", "clean"
+    system "swift", "build", "--disable-sandbox", "-c", "release"
     bin.install ".build/release/swiftvars"
   end
 
